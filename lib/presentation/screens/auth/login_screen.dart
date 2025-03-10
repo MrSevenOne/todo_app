68e59c9ec175de92d8fd5d10fd19c323c069bd4f
macbook <mirvoxidraxmonbekov@gmail.com>
2025-03-10 10:13:15 +0500

    login Screen UI

--- lib/presentation/screens/auth/login_screen.dart
+++ lib/presentation/screens/auth/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:provider/provider.dart';
 import 'package:todo_app/core/config/routes.dart';
 
 import '../../../core/config/constants.dart';
@@ -9,107 +10,115 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    TextEditingController _emailController = TextEditingController();
+    TextEditingController _passwordController = TextEditingController();
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.all(deffaultPadding),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisSize: MainAxisSize.max,
-            children: [
-              Text(
-                'Login',
-                style: Theme.of(context).textTheme.headlineLarge,
-              ),
-              SizedBox(height: 23.h),
-              Text(
-                'Username',
-                style: Theme.of(context).textTheme.bodyLarge,
-              ),
-              SizedBox(height: 8.h),
-              TextField(
-                decoration: InputDecoration(
-                  hintText: "Enter your Username",
-                ),
-                style: TextStyle(color: Colors.white),
-              ),
-              SizedBox(height: 20.h),
-              Text(
-                'Password',
-                style: Theme.of(context).textTheme.bodyLarge,
-              ),
-              SizedBox(height: 8.h),
-              TextField(
-                decoration: InputDecoration(
-                  hintText: "Enter your Password",
-                ),
-                style: TextStyle(color: Colors.white),
-              ),
-              SizedBox(height: 48.h),
-              SizedBox(
-                width: double.infinity,
-                height: 48.h,
-                child: ElevatedButton(
-                  onPressed: () {},
-                  child: Text(
+      body: Consumer(
+        builder: (context, provider, child) {
+          return SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.all(deffaultPadding),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Text(
                     'Login',
+                    style: Theme.of(context).textTheme.headlineLarge,
+                  ),
+                  SizedBox(height: 23.h),
+                  Text(
+                    'Email',
                     style: Theme.of(context).textTheme.bodyLarge,
                   ),
-                ),
-              ),
-              SizedBox(height: 15.h),
-              Divider(
-                color: Theme.of(context).colorScheme.outline,
-              ),
-              SizedBox(height: 20.h),
-              Container(
-                height: 48.h,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Theme.of(context).primaryColor,
-                    width: 2.0,
+                  SizedBox(height: 8.h),
+                  TextField(
+                    controller: _emailController,
+                    decoration: InputDecoration(
+                      hintText: "Enter your Email",
+                    ),
+                    style: TextStyle(color: Colors.white),
                   ),
-                  borderRadius: BorderRadius.circular(4.0),
-                ),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  spacing: 10.0,
-                  children: [
-                    Icon(Icons.g_mobiledata),
-                    Text("Login with google"),
-                  ],
-                ),
-              ),
-              SizedBox(height: 20.h),
-              Container(
-                height: 48.h,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Theme.of(context).primaryColor,
-                    width: 2.0,
+                  SizedBox(height: 20.h),
+                  Text(
+                    'Password',
+                    style: Theme.of(context).textTheme.bodyLarge,
                   ),
-                  borderRadius: BorderRadius.circular(4.0),
-                ),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  spacing: 10.0,
-                  children: [
-                    Icon(Icons.g_mobiledata),
-                    Text("Login with google"),
-                  ],
-                ),
+                  SizedBox(height: 8.h),
+                  TextField(
+                    controller: _passwordController,
+                    decoration: InputDecoration(
+                      hintText: "Enter your Password",
+                    ),
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  SizedBox(height: 48.h),
+                  SizedBox(
+                    width: double.infinity,
+                    height: 48.h,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Login',
+                        style: Theme.of(context).textTheme.bodyLarge,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 15.h),
+                  Divider(
+                    color: Theme.of(context).colorScheme.outline,
+                  ),
+                  SizedBox(height: 20.h),
+                  Container(
+                    height: 48.h,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Theme.of(context).primaryColor,
+                        width: 2.0,
+                      ),
+                      borderRadius: BorderRadius.circular(4.0),
+                    ),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      spacing: 10.0,
+                      children: [
+                        Icon(Icons.g_mobiledata),
+                        Text("Login with google"),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 20.h),
+                  Container(
+                    height: 48.h,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Theme.of(context).primaryColor,
+                        width: 2.0,
+                      ),
+                      borderRadius: BorderRadius.circular(4.0),
+                    ),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      spacing: 10.0,
+                      children: [
+                        Icon(Icons.g_mobiledata),
+                        Text("Login with google"),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 46.h),
+                ],
               ),
-              SizedBox(height: 46.h),
-            ],
-          ),
-        ),
+            ),
+          );
+        },
       ),
       bottomNavigationBar: Row(
         mainAxisAlignment: MainAxisAlignment.center,
@@ -126,7 +135,6 @@ class LoginScreen extends StatelessWidget {
               Navigator.pushNamed(
                 context,
                 AppRoutes.register,
-
               );
             },
             child: Text(

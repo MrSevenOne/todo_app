a6cfc03476e81cf826a60c52148b0c29da1835d2
macbook <mirvoxidraxmonbekov@gmail.com>
2025-04-01 16:04:36 +0500

    done main

--- lib/main.dart
+++ lib/main.dart
@@ -3,17 +3,22 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:provider/provider.dart';
 import 'package:todo_app/core/config/routes.dart';
+import 'package:todo_app/core/config/themes/light_theme.dart';
 import 'package:todo_app/core/utils/supabase_helper.dart';
 import 'package:todo_app/data/repositories/todo_repository.dart';
 import 'package:todo_app/presentation/provider/auth_provider.dart';
 import 'package:todo_app/presentation/provider/todo_provider.dart';
 import 'package:todo_app/presentation/provider/user_provider.dart';
+import 'package:todo_app/test.dart';
 import 'core/config/themes/dark_theme.dart';
 
 void main() async {
   //Connect with supabase
   WidgetsFlutterBinding.ensureInitialized();
   await SupabaseHelper.initializeSupabase();
+  //   final notificationService = SupabaseNotificationService();
+  // await notificationService.initNotifications();
+  // notificationService.listenForDatabaseChanges();
   runApp(
     ScreenUtilInit(
       designSize: Size(375, 812),

de75a1f4cf485efe697b6988937e564580f9bc98
macbook <mirvoxidraxmonbekov@gmail.com>
2025-04-01 15:32:06 +0500

    done Auth Service

--- lib/core/services/database/auth_service.dart
+++ lib/core/services/database/auth_service.dart
@@ -12,41 +12,44 @@ class AuthService {
 
   /// **Ro‘yxatdan o‘tish (E-mail & Parol)**
   Future<AuthResponse?> signUp({
-    required String name,
-    required String email,
-    required String password,
-  }) async {
-    try {
-      final response = await _supabase.auth.signUp(
+  required String name,
+  required String email,
+  required String password,
+}) async {
+  try {
+    final response = await _supabase.auth.signUp(
+      email: email,
+      password: password,
+    );
+
+    if (response.user != null) {
+      UserModel userModel = UserModel(
+        id: response.user!.id,
+        name: name,
         email: email,
         password: password,
+        created_at: response.user!.createdAt,
+        image_url: 'https://media.istockphoto.com/id/1300845620/vector/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=612x612&w=0&k=20&c=yBeyba0hUkh14_jgv1OKqIH0CCSWU_4ckRkAoy2p73o=', // 🖼 Default rasm URL
       );
-
-      if (response.user != null) {
-        UserModel userModel = UserModel(
-          id: response.user!.id,
-          name: name,
-          email: email,
-          password: password,
-          created_at: response.user!.createdAt,
-          image_url: 'hozircha null',
-        );
-        await userService.addUser(usermodel: userModel);
-      }
-
-      return response;
-    } on AuthException catch (e) {
-      if (e.message.contains('already registered')) {
-        SnackBarWidget.showInfo(
-            title: 'Registered', message: "This email is already registered!");
-      } else {
-        SnackBarWidget.showInfo(title: 'Error', message: "${e.message}");
-      }
-    } catch (e) {
-      SnackBarWidget.showInfo(title: 'Error registered', message: "$e");
-      return null;
+      await userService.addUser(usermodel: userModel);
     }
+ SnackBarWidget.showInfo(
+          title: 'Sign Up', message: 'You have successfully registered!');
+    return response;
+  } on AuthException catch (e) {
+    if (e.message.contains('already registered')) {
+      SnackBarWidget.showInfo(
+          title: 'Registered', message: "This email is already registered!");
+    } else {
+      SnackBarWidget.showInfo(title: 'Error', message: "${e.message}");
+    }
+  } catch (e) {
+    SnackBarWidget.showInfo(title: 'Error registered', message: "$e");
+    return null;
   }
+}
+
+     
 
 
   /// **Tizimga kirish (E-mail & Parol)**
@@ -59,6 +62,7 @@ class AuthService {
         email: email,
         password: password,
       );
+            SnackBarWidget.showInfo(title: 'Sign In', message: "You are logged in!");
     } on AuthException catch (e) {
       SnackBarWidget.showInfo(title: 'Error Login', message: "${e.message}");
     } catch (e) {
@@ -70,6 +74,8 @@ class AuthService {
   Future<void> signOut() async {
     try {
       await _supabase.auth.signOut();
+        SnackBarWidget.showInfo(
+          title: 'Sign Out', message: 'You are logged out!');
     } catch (e) {
       SnackBarWidget.showInfo(title: 'Error Logout', message: "${e}");
     }
